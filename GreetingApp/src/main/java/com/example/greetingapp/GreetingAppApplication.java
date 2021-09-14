a15f6493798075aac74212c1721aeff5037284d6
immanuveljeeva08 <immanuveljeeva2000@gmail.com>
2021-09-13 20:04:14 -0700

    Ability to return JSON for different HTTP methods

---
+++ GreetingApp/src/main/java/com/example/greetingapp/GreetingAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.greetingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to GreetingApp");
+        SpringApplication.run(GreetingAppApplication.class, args);
+    }
+
+}
